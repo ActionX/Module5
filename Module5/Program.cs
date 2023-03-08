6c00be2c899fa819e40c3171494bdc4b3bb75849
ActionX <irwin@bk.ru>
2023-03-08 19:35:12 +0700

    Final

--- Module5/Program.cs
+++ Module5/Program.cs
@@ -6,13 +6,13 @@ using System.Text;
 // + Фамилия;
 // + Возраст;
 // + Наличие питомца;
-// Если питомец есть, то запросить количество питомцев;
-// Если питомец есть, вызвать метод, принимающий на вход количество питомцев и возвращающий массив их кличек (заполнение с клавиатуры);
-// Запросить количество любимых цветов;
-// Вызвать метод, который возвращает массив любимых цветов по их количеству (заполнение с клавиатуры);
-// Сделать проверку, ввёл ли пользователь корректные числа: возраст, количество питомцев, количество цветов в отдельном методе;
-// Требуется проверка корректного ввода значений и повтор ввода, если ввод некорректен;
-// Корректный ввод: ввод числа типа int больше 0.
+// +Если питомец есть, то запросить количество питомцев;
+// +Если питомец есть, вызвать метод, принимающий на вход количество питомцев и возвращающий массив их кличек (заполнение с клавиатуры);
+// +Запросить количество любимых цветов;
+// +Вызвать метод, который возвращает массив любимых цветов по их количеству (заполнение с клавиатуры);
+// +Сделать проверку, ввёл ли пользователь корректные числа: возраст, количество питомцев, количество цветов в отдельном методе;
+// +Требуется проверка корректного ввода значений и повтор ввода, если ввод некорректен;
+// +Корректный ввод: ввод числа типа int больше 0.
 // Метод, который принимает кортеж из предыдущего шага и показывает на экран данные.
 // Вызов методов из метода Main.
 
@@ -25,75 +25,54 @@ class Program
         Console.OutputEncoding = Encoding.UTF8;
         Console.InputEncoding = Encoding.GetEncoding(1251);
 
-        var userData = GetUserInput();
+        var userData = GetUserData();
         ShowUserData(userData);
+
+        Console.ReadKey();
+
     }
 
 
-    static (string FirstName, string LastName, int Age, bool HasPets) GetUserInput()
+    static (string FirstName, string LastName, int Age, bool HasPets, string[] PetNames, string[] FavoriteColors) GetUserData()
     {
-        Console.Write("Имя: ");
+        Console.Write("Введите имя: ");
         var firstName = Console.ReadLine();
 
-        Console.Write("Фамилия: ");
+        Console.Write("Введите фамилию: ");
         var lastName = Console.ReadLine();
 
-		int age = GetPositiveInt("Возраст: ");
+		int age = GetPositiveInt("Введите возраст: ");
 
-        bool hasPets;
-        while (true)
-        {
-            Console.Write("Есть ли у вас питомец? (Да/Нет) ");
-            var hasPetsStr = Console.ReadLine().ToLower();
-            if (hasPetsStr == "да")
-            {
-                hasPets = true;
-                break;
-            }
-            else if (hasPetsStr == "нет")
-            {
-                hasPets = false;
-                break;
-            }
-            Console.WriteLine("Некорректный ввод. Попробуйте ещё раз.");
-        }
+        bool hasPets = GetHasPets();
 		
 		int numberOfPets;
-		string[] petNames;
-		if (hasPets) {
+		string[] petNames = new string[0];
+
+		if (hasPets)
+        {
 			numberOfPets = GetPositiveInt("Введите количество питомцев: ");
 			petNames = GetPetNames(numberOfPets);
 		}
 
-        //int colorCount;
-        //while (true)
-        //{
-        //    Console.Write("Введите количество любимых цветов: ");
-        //    if (int.TryParse(Console.ReadLine(), out colorCount) && colorCount > 0)
-        //    {
-        //        break;
-        //    }
-        //    Console.WriteLine("Некорректный ввод. Попробуйте ещё раз.");
-        //}
+        int favouriteColorCount = GetPositiveInt("Введите количество любимых цветов: ");
+        var favoriteColors = GetFavoriteColors(favouriteColorCount);
 
-        //var favoriteColors = GetFavoriteColors(colorCount);
-
-        return (FirstName: firstName, LastName: lastName, Age: age, HasPets: hasPets);
+        return (FirstName: firstName, LastName: lastName, Age: age, HasPets: hasPets, PetNames: petNames, FavoriteColors: favoriteColors);
     }
 
-    static void ShowUserData((string FirstName, string LastName, int Age, bool HasPets) userData)
+    static void ShowUserData((string FirstName, string LastName, int Age, bool HasPets, string[] PetNames, string[] FavoriteColors) userData)
     {
         Console.WriteLine($"Имя: {userData.FirstName}");
         Console.WriteLine($"Фамилия: {userData.LastName}");
         Console.WriteLine($"Возраст: {userData.Age}");
         Console.WriteLine($"Наличие питомца: {(userData.HasPets ? "да" : "нет")}");
-        //if (userData.Item4 && userData.Item5 != null)
-        //{
-        //    Console.WriteLine($"Количество питомцев: {userData.Item5.Length}");
-        //    Console.WriteLine($"Клички питомцев: {string.Join(", ", userData.Item5)}");
-        //}
-        //Console.WriteLine($"Количество любимых цветов: {userData.Item6.Length}");
-        //Console.WriteLine($"Любимые цвета: {string.Join(", ", userData.Item6)}");
+        if (userData.HasPets && userData.PetNames != null)
+        {
+            Console.WriteLine($"Количество питомцев: {userData.PetNames.Length}");
+            Console.WriteLine($"Клички питомцев: {string.Join(", ", userData.PetNames)}");
+        }
+        Console.WriteLine($"Количество любимых цветов: {userData.FavoriteColors.Length}");
+        Console.WriteLine($"Любимые цвета: {string.Join(", ", userData.FavoriteColors)}");
     }
 	
 	static int GetPositiveInt(string message)
@@ -108,20 +87,39 @@ class Program
 
 		return result;
 	}
-		
+	
+    static bool GetHasPets()
+    {
+        while (true)
+        {
+            Console.Write("Есть ли у вас питомец? (Да/Нет) ");
+            var hasPetsStr = Console.ReadLine().ToLower();
+            switch (hasPetsStr)
+            {
+                case "да":
+                    return true;
+                case "нет":
+                    return false;
+                default:
+                    break;
+            }
+            Console.WriteLine("Некорректный ввод. Попробуйте ещё раз.");
+        }
+    }
+
 	static string[] GetPetNames(int count)
 	{
 		var names = new string[count];
 		for (int i = 0; i < count; i++)
 		{
-			Console.Write($"Введите кличку питомца {i + 1}: ");
+			Console.Write($"Введите имя питомца {i + 1}: ");
 			names[i] = Console.ReadLine();
 		}
 
 		return names;
 	}
 
-	static string[] GetColors(int count)
+	static string[] GetFavoriteColors(int count)
 	{
 		var colors = new string[count];
 		for (int i = 0; i < count; i++)
